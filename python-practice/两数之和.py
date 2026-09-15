67eba4fe50a3f2faff16289d2a487704a0f5ca9a
MILING0107 <MILING0107@users.noreply.github.com>
2026-09-15 21:49:38 +0800

    优化两数之和的解题思路

--- python-practice/两数之和.py
+++ python-practice/两数之和.py
@@ -1,23 +1,25 @@
-def twoSum(nums, target):
-    lens = len(nums)
-    j=-1
-    for i in range(1,lens):
-        temp = nums[:i]
-        if (target - nums[i]) in temp:
-            j = temp.index(target - nums[i])
-            break
-    if j>=0:
-        return [j,i]
-    
+# def twoSum(nums,target):
+#     lens = len(nums)
+#     j = -1
+#     for i in range(1,lens):
+#         temp = nums[:i]
+#         if (target - nums[i]) in temp:
+#             j = temp.index(target - nums[i])
+#             break
+#     if j>=0:
+#         return[j,i]
+# 
+# res1 = twoSum([1,2,5,10,3,8,7],10)
+# print(res1)
+
+def twoSum(nums,target):
+    for i in range(len(nums)):
+        res = target - nums[i]
+        if res in nums[i+1:]:
+            j = nums[i+1:].index(res)+i+1
+            return[i,j]
+
 res1 = twoSum([1,2,5,10,3,8,7],10)
 print(res1)
 
-res2 = twoSum([3,3],6)
-print(res2)
-
-res3 = twoSum([2,7,11,15],9)
-print(res3)
-     
-            
-             
-     
+        
